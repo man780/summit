aa718dcb269c600a27e4f1892d9b6f39cc74199e
Murod Smith <man780@mail.ru>
2020-10-08 11:28:41 +0500

    Refs admin created

--- refs/admin.py
+++ refs/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import (
+    PreferDays,
+    PreferTimes,
+    Levels,
+    Statuses,
+    IsOld,
+    GroupTypes,
+    ComeFrom,
+    Sub
+)
 
-# Register your models here.
+
+@admin.register(PreferDays)
+class PreferDaysAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(PreferTimes)
+class PreferTimesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(Levels)
+class LevelsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(Statuses)
+class StatusesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(IsOld)
+class IsOldAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(GroupTypes)
+class GroupTypesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(ComeFrom)
+class ComeFromAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(Sub)
+class SubAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
