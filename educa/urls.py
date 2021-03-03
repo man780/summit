5d542e8ddf923f5bd2195d64fae6bc9851f26036
Murod Smith <man780@mail.ru>
2021-03-03 19:51:06 +0500

    media changed to static

--- educa/urls.py
+++ educa/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
 ] + static(settings.STATIC_URL,
            document_root=settings.STATIC_ROOT)
 
-if not settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
