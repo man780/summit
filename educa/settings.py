6d32f55156ebee923ec7e7f992fd127720d80e39
Murod Smith <man780@mail.ru>
2021-03-08 22:43:25 +0500

    1

--- educa/settings.py
+++ educa/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # STATIC_URL = '/live-static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 # STATIC_ROOT = os.path.join(BASE_DIR, "live-static")
