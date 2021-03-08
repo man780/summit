0e9b9d29f21051f45a6789afb791737f848b3fb0
Murod Smith <man780@mail.ru>
2021-03-08 20:59:28 +0500

    settings static_url corrected

--- educa/settings.py
+++ educa/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/live-static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 STATICFILES_DIRS = (
