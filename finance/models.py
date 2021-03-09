7c7df4a7f22fef9ec66d872c33cb6216671155c7
Murod Smith <man780@mail.ru>
2021-03-09 12:47:50 +0500

    Finance app configured

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from reception.models import Students
 
-# Create your models here.
+
+class Payment(models.Model):
+    STATUS = [
+        (0, 'Not paid'),
+        (1, 'Partial payment'),
+        (2, 'Full paid'),
+    ]
+    AMOUNT_TYPE = [
+        (1, 'Plastic'),
+        (2, 'Cash')
+    ]
+    date = models.DateField(auto_now_add=True)
+    student = models.ForeignKey(Students,
+                                related_name='payment_student',
+                                on_delete=models.CASCADE)
+    note = models.TextField()
+    amount = models.IntegerField(default=0)
+    type = models.IntegerField(choices=AMOUNT_TYPE, default=None, blank=True)
+    status = models.IntegerField(choices=STATUS)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.student.full_name()
+
+    class Meta:
+        ordering = ['created']
