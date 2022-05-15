c6e1dd40c232e8c2f61d37ee598757996a7fea90
m.tatybaev <m.tatybaev@qiwi.uz>
2022-05-15 17:58:17 +0500

    financial module added

--- finance/urls.py
+++ finance/urls.py
@@ -7,5 +7,6 @@ app_name = 'finance'
 urlpatterns = [
 
     path('', views.index, name='index'),
+    path('not-paid/', views.not_paid, name='not-paid'),
     path('statistics/', views.statistics, name='statistics'),
 ]
--- finance/views.py
+++ finance/views.py
@@ -36,6 +36,34 @@ def index(request):
                   })
 
 
+def not_paid(request):
+    students = Students.objects.all()
+    not_paid_students = []
+    for student in students:
+        try:
+            student = Payment.objects.get(student=student, status=0, amount=0)
+            # print(student)
+        except Payment.DoesNotExist:
+            not_paid_students.append(student)
+    # print(not_paid_students)
+    for student in not_paid_students:
+        print(student.full_name)
+
+    should_pay = []
+    for student in students:
+        try:
+            student = Payment.objects.filter(student=student, status=1).get()
+            should_pay.append(student)
+        except Payment.DoesNotExist:
+            pass
+            # should_pay.append(student)
+    return render(request,
+                  "finance/not_paid.html",
+                  {
+                      "not_paid_students": not_paid_students,
+                  })
+
+
 def statistics(request):
     result = (Students.objects
               .values('come_from')
